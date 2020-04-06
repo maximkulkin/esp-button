cf19a894c2e11e82ab7ad26a0805b6bc78c29301
Maxim Kulkin <maxim.kulkin@gmail.com>
2020-04-05 23:44:39 -0700

    Fix ESP-IDF build settings to allow building with Make

--- component.mk
+++ component.mk
@@ -1,6 +1,11 @@
-INC_DIRS += $(button_ROOT)
+ifdef component_compile_rules
 
-button_INC_DIR = $(button_ROOT)
-button_SRC_DIR = $(button_ROOT)
+    INC_DIRS += $(button_ROOT)
 
-$(eval $(call component_compile_rules,button))
+    button_INC_DIR = $(button_ROOT)
+    button_SRC_DIR = $(button_ROOT)
+
+    $(eval $(call component_compile_rules,button))
+else
+    COMPONENT_ADD_INCLUDEDIRS = .
+endif
